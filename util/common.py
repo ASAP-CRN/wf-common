55f6ebabc9650bb92e7eb8d0768813e442124390
Karen Fang <karen@dnastack.com>
2024-10-29 09:59:33 -0400

    Add report template

--- util/common.py
+++ util/common.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
 import subprocess
+from google.cloud import storage
 
 def list_teams():
 	print("Available teams:")
 	for team in ALL_TEAMS:
 		print(team)
+
+def list_gs_files(bucket_name, prefix=""):
+	client = storage.Client()
+	bucket = client.get_bucket(bucket_name)
+	blobs = bucket.list_blobs(prefix=prefix)
+	gs_files = [f"gs://{bucket_name}/{blob.name}" for blob in blobs]
+	return gs_files
+
+def 
 
 def gsync(source_path, destination_path, dry_run):
 	dry_run_arg = "-n" if dry_run else ""
