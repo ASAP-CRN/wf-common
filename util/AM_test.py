2c841a0b2a86e8169398def8c7ab3b2eda39c965
Alexander-Morin <alexander.morin@Alexander-Morin-MacBook-Pro.local>
2025-09-22 11:56:41 -0700

    testing local env works

---
+++ util/AM_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def test_am():
+    
+    df = pd.DataFrame({
+        'A': [1, 2, 3]
+    })
+
+    return df
+
+if __name__ == "__main__":
+    print(test_am())
