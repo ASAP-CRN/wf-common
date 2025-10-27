87bfd5cd65b9472e591cd33d6b01b32fa1446d91
Alexander-Morin <alexander.morin@Alexander-Morin-MacBook-Pro.local>
2025-10-27 12:20:34 -0700

    init local->raw bucket metadata transfer

---
+++ util/transfer_qc_metadata_to_raw_bucket.py
@@ -0,0 +1,99 @@
+# Transfer locally saved QC'd metadata to the raw Google bucket
+# Usage: python3 transfer_qc_metadata_to_raw_bucket.py -t jakobsson -ds pmdbs-bulk-rnaseq
+# Defaults to dry run unless -p flag is added!
+
+import argparse
+import logging
+import re
+import subprocess
+from pathlib import Path
+from common import gsync
+
+
+logging.basicConfig(
+	level=logging.INFO,
+	format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+repo_root = Path(__file__).resolve().parents[1]
+metadata_root = repo_root.parent / "asap-crn-cloud-dataset-metadata"
+
+
+def check_team_name(team_name: str) -> str:
+    """Strip 'team' prefix if present"""
+    norm = re.sub(r'^\s*team[-_ ]*', '', team_name.strip(), flags=re.IGNORECASE)
+    norm = norm.strip().lower()
+    if not norm:
+        raise ValueError("team_name is empty after stripping 'team' prefix")
+    return norm
+
+
+def bucket_exists(bucket_url: str) -> None:
+    command = ["gcloud", "storage", "buckets", "describe", bucket_url]
+    try:
+        subprocess.run(command, check=True, capture_output=True, text=True)
+    except subprocess.CalledProcessError as e:
+        raise ValueError(f"Bucket not found: {bucket_url}, see: {e}")
+
+
+def main(args):
+
+    dry_run = not args.promote
+    
+    team_name = check_team_name(args.team_name)
+    dataset_name = args.dataset_name
+    dataset_name_long = f"{team_name}-{dataset_name}"
+    
+    dataset_dir = metadata_root / "datasets" / dataset_name_long
+    metadata_dir = dataset_dir / "metadata"
+    file_metadata_dir = dataset_dir / "file_metadata"
+    
+    bucket_name = f"gs://asap-raw-team-{team_name}-{dataset_name}"
+    # bucket_name = "gs://test_bucket_alexm"
+    # bucket_name = "gs://asap-raw-team-jakobsson-pmdbs-bulk-rnaseq"
+    file_metadata_bucket = f"{bucket_name}/file_metadata"
+    metadata_bucket = f"{bucket_name}/metadata"
+    
+    bucket_exists(bucket_name)
+
+    if file_metadata_dir.exists():
+        logging.info(f"Transferring local file_metadata directory to [{file_metadata_bucket}]")
+        gsync(source_path=file_metadata_dir, destination_path=file_metadata_bucket, dry_run=dry_run)
+    else:
+        raise ValueError(f"Local file metadata directory not found: {file_metadata_dir}")
+    
+    if metadata_dir.exists():
+        logging.info(f"Transferring local metadata directory to [{metadata_bucket}]")
+        gsync(source_path=metadata_dir, destination_path=metadata_bucket, dry_run=dry_run)
+    else:
+        raise ValueError(f"Local metadata directory not found: {metadata_dir}")
+    
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(
+		description="Sync local metadata directories to raw bucket"
+	)
+    
+    parser.add_argument(
+        "-t",
+        "--team_name",
+        required=True,
+        help="The team name of the dataset"
+    )
+    parser.add_argument(
+        "-ds",
+        "--dataset_name",
+        required=True,
+        help="The name of the dataset"
+    )
+    parser.add_argument(
+        "-p",
+		"--promote",
+		action="store_true",
+		required=False,
+		help="Promote data (default is dry run)."
+	)
+
+    args = parser.parse_args()
+    main(args)
